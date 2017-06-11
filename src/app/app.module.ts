e837bbe5e289e1474e2e81180e24eda8b522720b
Hyunmin Choi <pointer81@gmail.com>
2017-06-11 15:32:30 +0900

    Router works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HttpModule } from '@angular/http';
 
 import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { DealsMapComponent } from './deals-map/deals-map.component';
+import { DealsListComponent } from './deals-list/deals-list.component';
+
+
+const appRoutes: Routes = [
+  { path: 'map', component: DealsMapComponent },
+  { path: 'list', component: DealsListComponent },
+  { path: '**', component: DealsListComponent },
+];
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        DealsMapComponent,
+        DealsListComponent,
     ],
     imports: [
+        RouterModule.forRoot(appRoutes, { useHash: false }),
         BrowserModule,
         HttpModule,
         AgmCoreModule.forRoot({
