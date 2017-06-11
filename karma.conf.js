78050713532fc47a3c5757284220927d660c16ad
Hyunmin Choi <pointer81@gmail.com>
2017-06-11 20:58:18 +0900

    Let it show messages in terminal

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,11 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    browserConsoleLogOptions: {
+      level: 'log',
+      format: '%b %T: %m',
+      terminal: true
+    }
   });
 };
