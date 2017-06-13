9a71d75f762eddadf81d5719e26e005bb3eef795
Hyunmin Choi <pointer81@gmail.com>
2017-06-13 10:37:29 +0900

    Tests OK without router

--- src/app/deals-list/deals-list.component.ts
+++ src/app/deals-list/deals-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router }   from '@angular/router';
 import { DealsService } from '../deals.service';
 import 'rxjs/add/operator/pluck';
 
@@ -13,19 +12,14 @@ export class DealsListComponent implements OnInit {
     public deals = [];
 
     constructor(private _deals_service: DealsService,
-                private route:  ActivatedRoute,
-                private router: Router) {
+) {
     }
 
     ngOnInit() {
-        this.route.params.subscribe(p => this.getDeals(p && p['deal_id']));
-        // this.route.params.pluck<string>('deal_id')
-        //     .forEach(deal_id => this.getDeals(deal_id));
     }
 
     getDeals(deal_id: string) {
-        console.log('get deals()');
-        this._deals_service.getDeals(deal_id).subscribe(
+        this._deals_service.getDeals().subscribe(
             (deals) => {
                 console.log('deals in DealsListcomponent', deals);
                 this.deals = deals.results;
